9e05b1b1eb69ec37179f4a9a81f36dc78f7b4f00
Stoycho Donev <stoychod79@gmail.com>
2023-04-28 06:14:32 +0100

    refactor: add typescript to the project

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.get("/user", (req, res) => {
+  res.send("Hello, Stoycho");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
