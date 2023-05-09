ce8179a5d84677458b5a00760b13676b6dd61697
Stoycho Donev <stoychod79@gmail.com>
2023-05-09 06:37:54 +0100

    refactor: remove unnecessary catch caluses in db.user

--- src/db/user.ts
+++ src/db/user.ts
@@ -4,35 +4,22 @@ const user = {
   create: async (userData: string[]) => {
     const statement =
       "INSERT INTO users(email, password, first_name, last_name) VALUES($1, $2, $3, $4) RETURNING *";
-    try {
-      console.log(userData);
-      const result = await pool.query(statement, userData);
-      if (result.rows?.length) {
-        console.log(result.rows[0]);
-        return result.rows[0];
-      }
-
-      return null;
-    } catch (error) {
-      console.log(error);
-      throw error;
+    const result = await pool.query(statement, userData);
+    if (result.rows?.length) {
+      return result.rows[0];
     }
+
+    return null;
   },
 
   findByOneEmail: async (email: string) => {
     const statement = "SELECT * FROM users WHERE email = $1";
-    try {
-      const result = await pool.query(statement, [email]);
-      if (result.rows?.length) {
-        console.log(result.rows[0]);
-        return result.rows[0];
-      }
-
-      return null;
-    } catch (error) {
-      console.log(error);
-      throw error;
+    const result = await pool.query(statement, [email]);
+    if (result.rows?.length) {
+      return result.rows[0];
     }
+
+    return null;
   },
 };
 
