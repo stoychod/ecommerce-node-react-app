dd731eaf03c372d10930cb66b3e59017133e0cd9
Stoycho Donev <stoychod79@gmail.com>
2023-05-21 11:33:09 +0100

    fix: expect either HttpError or Error object in error middleware

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -3,14 +3,18 @@ import logger from "../logger/logger";
 import { Request, Response, NextFunction } from "express";
 
 const errorMiddleware = (
-  err: HttpError,
+  err: HttpError | Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   logger.error(err);
-  const { status, message } = err;
-  return res.status(status).send({ message });
+  if (err instanceof HttpError) {
+    const { status, message } = err;
+    return res.status(status).send({ message });
+  }
+
+  return res.status(500).send({ message: "Server error" });
 };
 
 export default errorMiddleware;
