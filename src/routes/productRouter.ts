c912bafe8786e0f4ce126c3106882513f860d258
Stoycho Donev <stoychod79@gmail.com>
2023-05-22 11:55:46 +0100

    feat: add /products route to productRouter

--- src/routes/productRouter.ts
+++ src/routes/productRouter.ts
@@ -6,7 +6,20 @@ const router = express.Router();
 const productRouter = (app: Application, db: Pool) => {
   const productService = new ProductService(db);
 
-  app.use("/product", router);
+  app.use("/products", router);
+
+  router.get("/", async (req, res, next) => {
+    try {
+      const category =
+        typeof req.query.category === "string" ? req.query.category : null;
+
+      const products = await productService.listProducts(category);
+
+      res.status(200).send(products);
+    } catch (error) {
+      next(error);
+    }
+  });
 
   router.get("/:id", async (req, res, next) => {
     try {
