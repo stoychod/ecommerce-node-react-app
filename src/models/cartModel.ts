a3c2c22c80bc608bfd46fe370d5c2611bf4169e5
Stoycho Donev <stoychod79@gmail.com>
2023-05-25 13:27:39 +0100

    feat: add a cartModel module

---
+++ src/models/cartModel.ts
@@ -0,0 +1,41 @@
+import { Pool } from "pg";
+
+export default class CartModel {
+  db: Pool;
+  constructor(db: Pool) {
+    this.db = db;
+  }
+
+  async create(userId: string) {
+    const statement = "INSERT INTO cart(users_id, total) $1, $2";
+    const result = await this.db.query(statement, [userId, 0]);
+
+    if (result.rows?.length) {
+      return result.rows[0];
+    }
+
+    return null;
+  }
+
+  async findOneByUserId(userId: string) {
+    const statement = "SELECT * FROM cart WHERE users_id = $1";
+    const result = await this.db.query(statement, [userId]);
+
+    if (result.rows?.length) {
+      return result.rows;
+    }
+
+    return null;
+  }
+
+  async findOneById(cartId: string) {
+    const statement = "SELECT * FROM cart WHERE id = $1";
+    const result = await this.db.query(statement, [cartId]);
+
+    if (result.rows?.length) {
+      return result.rows;
+    }
+
+    return null;
+  }
+}
