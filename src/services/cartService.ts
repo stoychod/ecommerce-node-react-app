21cfea86599046387e8fda70d7c4dd88c7b3a395
Stoycho Donev <stoychod79@gmail.com>
2023-05-26 16:59:17 +0100

    feat: add addItem method to the CartService class

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,10 +1,14 @@
 import { Pool } from "pg";
 import CartModel from "../models/cartModel";
+import CartItemModel from "../models/cartItemModel";
+import createHttpError from "http-errors";
 
 export default class CartService {
   cartModel: CartModel;
+  cartItemModel: CartItemModel;
   constructor(db: Pool) {
     this.cartModel = new CartModel(db);
+    this.cartItemModel = new CartItemModel(db);
   }
 
   async createCart(userId: string) {
@@ -33,5 +37,21 @@ export default class CartService {
       }
     }
   }
+
+  async addItem(userId: string, data: { productId: string; quantity: number }) {
+    try {
+      const cart = await this.cartModel.findOneByUserId(userId);
+
+      const cartItem = await this.cartItemModel.create({
+        cartId: cart.id,
+        ...data,
+      });
+
+      return cartItem;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw createHttpError(500, "Server error");
+      }
+    }
   }
 }
