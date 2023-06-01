9fbb3167e6d9e0fc6344439f2dfc107daa754813
Stoycho Donev <stoychod79@gmail.com>
2023-06-01 17:22:21 +0100

    feat: add an update method to UserModel class

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,4 +1,5 @@
 import { Pool } from "pg";
+import bcrypt from "bcrypt";
 
 export default class UserModel {
   db: Pool;
@@ -9,6 +10,46 @@ export default class UserModel {
     const statement =
       "INSERT INTO users(email, password, first_name, last_name) VALUES($1, $2, $3, $4) RETURNING *";
     const result = await this.db.query(statement, userData);
+    if (result.rows?.length) {
+      return result.rows[0];
+    }
+
+    return null;
+  }
+
+  async update(userData: {
+    id: string;
+    email?: string;
+    password?: string;
+    firstName?: string;
+    lastName?: string;
+  }) {
+    console.log(userData);
+
+    const { id, email, firstName, lastName } = userData;
+
+    // if password is being changed hash it before saving to database
+    let password = userData.password;
+    if (password) {
+      const saltRounds = 10;
+      password = await bcrypt.hash(password, saltRounds);
+    }
+
+    const statement = `UPDATE users
+                       SET email = COALESCE($2, email),
+                           password = COALESCE($3, password),
+                           first_name = COALESCE($4, first_name),
+                           last_name = COALESCE($5, last_name)
+                       WHERE id = $1
+                       RETURNING *`;
+
+    const result = await this.db.query(statement, [
+      id,
+      email,
+      password,
+      firstName,
+      lastName,
+    ]);
     if (result.rows?.length) {
       return result.rows[0];
     }
